f736bf9b7d0c2205c4ad36081fe32f0324b39697
Arafat37 <arafatsarkar22@gmail.com>
2022-04-04 03:22:57 +0600

    added more charts

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area, BarChart, Bar } from 'recharts';
 
 
 const Dashboard = () => {
@@ -12,28 +12,47 @@ const Dashboard = () => {
   console.log(chartData)
   return (
     <div>
-      
-      {/* <ResponsiveContainer width="100%" height="100%"> */}
-        <LineChart
-            width={500}
-            height={300}
-            data={chartData}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="month" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
-          </LineChart>
-        {/* </ResponsiveContainer> */}
-
+      <h1 className='text-3xl font-bold underline text-center mb-8'>DashBoard</h1>
+      <div className='grid lg:grid-cols-2 justify-items-center'>
+        <div >
+          <AreaChart
+              width={800}
+              height={500}
+              data={chartData}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Area type="monotone" dataKey="sell" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
+          </div>
+          <div>
+              {/* <ResponsiveContainer width="100%" height="100%"> */}
+                <LineChart
+                  width={800}
+                  height={500}
+                  data={chartData}
+                  >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="month" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
+                </LineChart>
+              {/* </ResponsiveContainer> */}
+          </div>
+          <div>
+          <BarChart width={800} height={500} data={chartData}>
+          <Bar dataKey="sell" fill="#8884d8" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+        </BarChart>
+          </div>
+        </div>
     </div>
   );
 };
