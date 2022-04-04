f54fe6f0d18c4368e2364b39289c24b52623e170
Arafat37 <arafatsarkar22@gmail.com>
2022-04-05 04:42:35 +0600

    responsive charts

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area, BarChart, Bar, ComposedChart, Scatter, PieChart, Pie, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar } from 'recharts';
+import './Dashboard.css'
 
 
 const Dashboard = () => {
@@ -15,22 +16,29 @@ const Dashboard = () => {
       <div className='grid lg:grid-cols-2 justify-items-center'>
         <div className='mb-10'>
           <h1 className='text-3xl font-bold text-center mb-3 underline'>Invest vs Revenue</h1>
-        <ComposedChart
-          width={800}
-          height={500}
-          data={chartData}
-        >
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="month" scale="band" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="investment" barSize={20} fill="#413ea0" />
-          <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
-        </ComposedChart>
+          <div className='chart-div'>
+            <ResponsiveContainer>
+              <ComposedChart
+                width={500}
+                height={400}
+                data={chartData}
+              >
+                <CartesianGrid stroke="#f5f5f5" />
+                <XAxis dataKey="month" scale="band" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Area type="monotone" dataKey="revenue" fill="#8884d8" stroke="#8884d8" />
+                <Bar dataKey="investment" barSize={20} fill="#413ea0" />
+                <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+              </ComposedChart>
+            </ResponsiveContainer>
+           </div>
           </div>
           <div className='mb-10'>
             <h1 className='text-3xl font-bold text-center underline mb-3'>Month wise Sell</h1>
+            <div className='chart-div'>
+              <ResponsiveContainer>
                 <LineChart
                   width={800}
                   height={500}
@@ -43,28 +51,38 @@ const Dashboard = () => {
                   <Legend />
                   <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
                 </LineChart>
+              </ResponsiveContainer>
+            </div>
           </div>
           <div className='mb-10'>
             <h1 className='text-3xl font-bold text-center underline mb-3'>Month wise Sell</h1>
-          <BarChart width={800} height={500} data={chartData}>
-          <Bar dataKey="sell" fill="#8884d8" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          </BarChart>
+              <div className='chart-div'>
+                <ResponsiveContainer>
+                <BarChart width={800} height={500} data={chartData}>
+                  <Bar dataKey="sell" fill="#8884d8" />
+                  <XAxis dataKey="month" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                </BarChart>
+                </ResponsiveContainer>
+              </div>
         </div>
         <div>
           <h1 className='text-3xl font-bold text-center underline'>Invest vs Revenue</h1>
-          <RadarChart width={800}
-          height={600} cx="50%" cy="50%" outerRadius="80%" data={chartData}>
-            <PolarGrid />
-            <PolarAngleAxis dataKey="month" />
-            <PolarRadiusAxis />
-            <Radar name="investment" dataKey="investment" stroke="#ff7300" fill="#ff7300" fillOpacity={0.6} />
-            <Radar name="revenue" dataKey="revenue" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-            <Legend />
-          </RadarChart>
+          <div className='chart-div'>
+            <ResponsiveContainer>
+              <RadarChart width={800}
+              height={600} cx="50%" cy="50%" outerRadius="80%" data={chartData}>
+                <PolarGrid />
+                <PolarAngleAxis dataKey="month" />
+                <PolarRadiusAxis />
+                <Radar name="investment" dataKey="investment" stroke="#ff7300" fill="#ff7300" fillOpacity={0.6} />
+                <Radar name="revenue" dataKey="revenue" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                <Legend />
+              </RadarChart>
+          </ResponsiveContainer>
+          </div>
         </div>
         </div>
     </div>
